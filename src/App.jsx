f1295ccdab6fd85c31d5a2c40ea263fdd4f22f6b
Madesh-Aadithya <madeshaadithyasb@gmail.com>
2026-08-07 12:32:47 +0000

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">
-        My Portfolio
-      </h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
