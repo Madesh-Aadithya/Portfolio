34378a6f4c14632cbbe1e71f230687026f2dc1c0
Madesh-Aadithya <madeshaadithyasb@gmail.com>
2026-08-08 06:21:16 +0000

    Added Mainlayout.jsx and modified App.jsx and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Home Page</h1>
-    </main>
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        My Portfolio
+      </h1>
+    </section>
   );
 }
 
